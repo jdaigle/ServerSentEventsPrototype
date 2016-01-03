4d6fa859fff9857b631c8876fd5ffc2263c5bcfa
Joseph Daigle <joseph.daigle@gmail.com>
2016-01-03 14:18:37 -0500

    completed prototype

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# ServerSentEventsPrototype
+This project is a simple prototype of [Server-sent events](https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events) using ASP.NET and some simple JavaScript.
+
+Note that this only works in Chrome/FireFox and requires a polyfill for IE.
+
+Server-sent events is basically a more mature version of long-polling or [Comet](https://en.wikipedia.org/wiki/Comet_(programming)) like model. It provides a simple API exposed through the [EventSource](https://developer.mozilla.org/en-US/docs/Web/API/EventSource) object. In essence, the UA will open a connection to a URL with ACCEPT header of `text/event-stream`. The connection stays open on the server, with the server writing events back when available. Events can be named, or unnamed (called messages). Events can also have a corresponding ID. If the connection is lost, either by the UA or the server, the UA will retry after waiting three seconds. It'll include the last event ID in the header if applicable.
+
+And that's it.
+
+The server should, of course, correctly implement asynchronous and non-blocking behavior to prevent worker thread starvation.
+
+It'll be interesting to see how this approach scales. And how it works with load balancers and other proxies. It's also untested on mobile.
---
+++ ServerSentEventsPrototype/Controllers/ServerTimeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServerSentEventsPrototype.Controllers
+{
+    public class ServerTimeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            long lastEventID = 0;
+            var lastEventIDPresent = long.TryParse(Request.Headers["Last-Event-ID"], out lastEventID);
+
+            Response.ContentType = "text/event-stream";
+            Response.Flush();
+
+            await Task.Delay(5000);
+
+            var currentdate = DateTime.UtcNow;
+            while (currentdate.AddSeconds(10) > DateTime.UtcNow)
+            {
+                lastEventID++;
+                var now = DateTime.UtcNow;
+                Response.Write("event: serverTime\n");
+                Response.Write(string.Format("id: {0}\n", lastEventID.ToString()));
+                Response.Write(string.Format("data: {0} - {1}\n", now.ToString("HH:mm:ss", CultureInfo.InvariantCulture), lastEventID.ToString()));
+                Response.Write("\n");
+                Response.Flush();
+                await Task.Delay(1000);
+            }
+            return Content("");
+        }
+    }
+}
